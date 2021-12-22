2c30e7a7e6c46c5c7fb98b62bdfc0c4b9f63b071
chris <ct.king2019@gmail.com>
2021-12-22 17:30:09 -0500

    added emergency circuit all

--- FB_Beacons_PilotLights.py
+++ FB_Beacons_PilotLights.py
@@ -1,6 +1,8 @@
 
+
+## config var
 epc_dict = {
-             #'BCN1_13_000_0000' : 'AE13',   hardcode it using the ae13 variable
+
              'EPC_1E33001H' : 'EPC_13_100_0040',
              'EPC_133001X'  : 'EPC_13_100_0110',
              'EPC_133019C'  : 'EPC_13_100_0235',
@@ -19,9 +21,44 @@ epc_dict = {
              'EPC_133075D'  : 'EPC_13_700_0025',
              'EPC_133070 '  : 'EPC_13_701_0035'
              }
+sg_List = ['100',
+           '101',
+           '102',
+           '103',
+           '104',
+           '105',
+           '106',
+           '107',
+           '108',
+           '109',
+           '110',
+           '600',
+           '601',
+           '700',
+           '701'
+            ]
+
+
 ae = 13
 
+
+
+#######
 with open('outputFB_Pilot_Beacon.txt', 'w') as f:
+
+
+    f.write(f'EmergencyCircuitOK_All := SafetyOutput.EmergencyCircuitOk.AE{ae} \n')
+    for sg in sg_List:
+        if sg == sg_List[-1]:
+            f.write(f'                AND SafetyOutput.EmergencyCircuitOk.SG{sg}; \n \n \n \n ')
+        else:
+            f.write(f'                AND SafetyOutput.EmergencyCircuitOk.SG{sg} \n ')
+
+
+
+
+
+
 
     f.write(f'Mapping_Cabinet_IO.AE{ae}_Panel.Outputs.BCN1_{ae}_000_0000.Beacon_Red:=  SafetyOutput.EmergencyActivated.AE{ae}  AND fbBlink_500ms.Pulse; \n')
     for layoutNum in epc_dict.keys():
